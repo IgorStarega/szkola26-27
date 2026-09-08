99186e11b7b959cc56d186fde96aaf57d73b7534
Igor Staręga <staregaigor@gmail.com>
2026-09-08 11:19:16 +0200

    opa

---
+++ DESKTOPY/Pieski/script.js
@@ -0,0 +1,24 @@
+function pies1() {
+    zdjecie = document.getElementById("zdjecie");
+    opis = document.getElementById("opis");
+    opis.innerHTML = "To jest pies nr 1.";
+    zdjecie.innerHTML = "<img class='powiekszony' src='img/pies1.jpg' alt='pies1' width='100%'>";
+}
+function pies2() {
+    opis = document.getElementById("opis");
+    opis.innerHTML = "To jest pies nr 2.";
+    zdjecie = document.getElementById("zdjecie");
+    zdjecie.innerHTML = "<img class='powiekszony' src='img/pies2.jpg' alt='pies2' width='100%'>";
+}
+function pies3() {
+    opis = document.getElementById("opis");
+    opis.innerHTML = "To jest pies nr 3.";
+    zdjecie = document.getElementById("zdjecie");
+    zdjecie.innerHTML = "<img class='powiekszony' src='img/pies3.jpg' alt='pies3' width='100%'>";
+}
+function pies4() {
+    opis = document.getElementById("opis");
+    opis.innerHTML = "To jest pies nr 4.";
+    zdjecie = document.getElementById("zdjecie");
+    zdjecie.innerHTML = "<img class='powiekszony' src='img/pies4.jpg' alt='pies4' width='100%'>";
+}
